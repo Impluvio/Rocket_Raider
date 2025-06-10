d95bd3668e93e888c5d6be91428270713ff88527
Impluvio <adamcorneliustuffey@gmail.com>
2025-06-10 19:49:46 +0100

    setting up locomotion script.

--- Assets/Scripts/Locomotion.cs
+++ Assets/Scripts/Locomotion.cs
@@ -1,22 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 public class Locomotion : MonoBehaviour
 {
-    [SerializeField] KeyCode left;
-    [SerializeField] KeyCode right;
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 4f;
+    [SerializeField] InputAction rotation;
 
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody rb;
+
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
+    {
+        ApplyThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
     {
         
     }
+
+    private void ApplyThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            // Vector3 newForce = new Vector3(0,10,0);
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+
+
 }
